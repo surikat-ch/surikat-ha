4508dfaa5bd22bada65c9835ae6597ca79f9172d
Samuel Goumaz <samuel@atelierharfang.ch>
2025-12-17 03:08:57 +0100

    Fix

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-# Script principal qui est exécuté quand l’add-on démarre.
-# Peut :
-# Lancer des services
-# Préparer l’environnement
-# Appliquer des configurations spécifiques
-# Souvent le point d’entrée défini dans le Dockerfile.
+#! Script principal qui est exécuté quand l’add-on démarre.
+#! Peut :
+#! Lancer des services
+#! Préparer l’environnement
+#! Appliquer des configurations spécifiques
+#! Souvent le point d’entrée défini dans le Dockerfile.
 
 #!/usr/bin/with-contenv bashio
 
